20a2a91d15fab78be296ecd0d4ea2d815d4f0377
SheerBuffalo <alanr.sanchez512@gmail.com>
2023-06-26 16:41:55 -0600

    ahora se pintan los numeros de las casillas

--- cliente.py
+++ cliente.py
@@ -16,48 +16,66 @@ colores = {
 }
 
 def dibujarTablero():
+	numero_casilla=0
 	for i in range(8):
-		dibujoCuadrado(55)
+		numero_casilla+=1
+		dibujoCuadrado(55, numero_casilla)
 	skk.left(45)
-	dibujoCuadrado(35)
+	numero_casilla+=1
+	dibujoCuadrado(35, numero_casilla)
 	skk.left(45)
 	for i in range(6):
-		dibujoCuadrado(55)
+		numero_casilla+=1
+		dibujoCuadrado(55, numero_casilla)
 	skk.left(45)
-	dibujoCuadrado(35)
+	numero_casilla+=1
+	dibujoCuadrado(35, numero_casilla)
 	skk.left(45)
 	for i in range(9):
-		dibujoCuadrado(55)
+		numero_casilla+=1
+		dibujoCuadrado(55, numero_casilla)
 	skk.left(45)
-	dibujoCuadrado(35)
+	numero_casilla+=1
+	dibujoCuadrado(35, numero_casilla)
 	skk.left(45)
 	for i in range(5):
-		dibujoCuadrado(55)
+		numero_casilla+=1
+		dibujoCuadrado(55, numero_casilla)
 	skk.left(45)
-	dibujoCuadrado(35)
+	numero_casilla+=1
+	dibujoCuadrado(35, numero_casilla)
 	skk.left(45)
 	for i in range(8):
-		dibujoCuadrado(55)
+		numero_casilla+=1
+		dibujoCuadrado(55, numero_casilla)
 	skk.left(45)
-	dibujoCuadrado(35)
+	numero_casilla+=1
+	dibujoCuadrado(35, numero_casilla)
 	skk.left(45)
 	for i in range(4):
-		dibujoCuadrado(55)
+		numero_casilla+=1
+		dibujoCuadrado(55, numero_casilla)
 	skk.left(45)
-	dibujoCuadrado(35)
+	numero_casilla+=1
+	dibujoCuadrado(35, numero_casilla)
 	skk.left(45)
 	for i in range(7):
-		dibujoCuadrado(55)
+		numero_casilla+=1
+		dibujoCuadrado(55, numero_casilla)
 	skk.left(45)
-	dibujoCuadrado(35)
+	numero_casilla+=1
+	dibujoCuadrado(35, numero_casilla)
 	skk.left(45)
 	for i in range(3):
-		dibujoCuadrado(55)
+		numero_casilla+=1
+		dibujoCuadrado(55, numero_casilla)
 	skk.left(45)
-	dibujoCuadrado(35)
+	numero_casilla+=1
+	dibujoCuadrado(35, numero_casilla)
 	skk.left(45)
 	for i in range(4):
-		dibujoCuadrado(55)
+		numero_casilla+=1
+		dibujoCuadrado(55, numero_casilla)
 	skk.right(90)
 	skk.fd(55)
 	skk.left(90)
@@ -67,8 +85,15 @@ def dibujarTablero():
 		skk.forward(100)
 		skk.left(90)
 	skk.end_fill()
+	skk.left(45)
+	skk.penup()
+	skk.forward(50)
+	skk.write("63")
+	skk.backward(50)
+	skk.right(45)
+	skk.pendown()
 	skk.forward(100)
-
+	
 def IniciarJuego():
 	tableroInicial=wn
 	skk.penup()
@@ -77,7 +102,7 @@ def IniciarJuego():
 	client_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	client_socket.connect(('192.168.0.200', 12345))
 
-def dibujoCuadrado(tamano_casilla):
+def dibujoCuadrado(tamano_casilla, i):
 
 	skk.fillcolor(random.choice(list(colores.keys())))
 	skk.begin_fill()
@@ -85,7 +110,15 @@ def dibujoCuadrado(tamano_casilla):
 		skk.forward(tamano_casilla)
 		skk.right(90)
 	skk.end_fill()
+	skk.right(45)
+	skk.penup()
+	skk.fd(tamano_casilla/2)
+	skk.write(i)
+	skk.left(180)
+	skk.forward(tamano_casilla/2)
+	skk.right(135)
 	skk.forward(tamano_casilla)
+	skk.pendown()
 
 
 
--- servidor.py
+++ servidor.py
@@ -16,48 +16,66 @@ colores = {
 }
 
 def dibujarTablero():
+	numero_casilla=0
 	for i in range(8):
-		dibujoCuadrado(55)
+		numero_casilla+=1
+		dibujoCuadrado(55, numero_casilla)
 	skk.left(45)
-	dibujoCuadrado(35)
+	numero_casilla+=1
+	dibujoCuadrado(35, numero_casilla)
 	skk.left(45)
 	for i in range(6):
-		dibujoCuadrado(55)
+		numero_casilla+=1
+		dibujoCuadrado(55, numero_casilla)
 	skk.left(45)
-	dibujoCuadrado(35)
+	numero_casilla+=1
+	dibujoCuadrado(35, numero_casilla)
 	skk.left(45)
 	for i in range(9):
-		dibujoCuadrado(55)
+		numero_casilla+=1
+		dibujoCuadrado(55, numero_casilla)
 	skk.left(45)
-	dibujoCuadrado(35)
+	numero_casilla+=1
+	dibujoCuadrado(35, numero_casilla)
 	skk.left(45)
 	for i in range(5):
-		dibujoCuadrado(55)
+		numero_casilla+=1
+		dibujoCuadrado(55, numero_casilla)
 	skk.left(45)
-	dibujoCuadrado(35)
+	numero_casilla+=1
+	dibujoCuadrado(35, numero_casilla)
 	skk.left(45)
 	for i in range(8):
-		dibujoCuadrado(55)
+		numero_casilla+=1
+		dibujoCuadrado(55, numero_casilla)
 	skk.left(45)
-	dibujoCuadrado(35)
+	numero_casilla+=1
+	dibujoCuadrado(35, numero_casilla)
 	skk.left(45)
 	for i in range(4):
-		dibujoCuadrado(55)
+		numero_casilla+=1
+		dibujoCuadrado(55, numero_casilla)
 	skk.left(45)
-	dibujoCuadrado(35)
+	numero_casilla+=1
+	dibujoCuadrado(35, numero_casilla)
 	skk.left(45)
 	for i in range(7):
-		dibujoCuadrado(55)
+		numero_casilla+=1
+		dibujoCuadrado(55, numero_casilla)
 	skk.left(45)
-	dibujoCuadrado(35)
+	numero_casilla+=1
+	dibujoCuadrado(35, numero_casilla)
 	skk.left(45)
 	for i in range(3):
-		dibujoCuadrado(55)
+		numero_casilla+=1
+		dibujoCuadrado(55, numero_casilla)
 	skk.left(45)
-	dibujoCuadrado(35)
+	numero_casilla+=1
+	dibujoCuadrado(35, numero_casilla)
 	skk.left(45)
 	for i in range(4):
-		dibujoCuadrado(55)
+		numero_casilla+=1
+		dibujoCuadrado(55, numero_casilla)
 	skk.right(90)
 	skk.fd(55)
 	skk.left(90)
@@ -67,6 +85,13 @@ def dibujarTablero():
 		skk.forward(100)
 		skk.left(90)
 	skk.end_fill()
+	skk.left(45)
+	skk.penup()
+	skk.forward(50)
+	skk.write("63")
+	skk.backward(50)
+	skk.right(45)
+	skk.pendown()
 	skk.forward(100)
 
 def IniciarServidor():
@@ -74,14 +99,14 @@ def IniciarServidor():
 	skk.penup()
 	skk.goto(-170, -170)  # Posición inicial del jugador
 	skk.pendown()
-	server_socket = socket(socket.AF_INET, socket.SOCK_STREAM)
+	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server_socket.bind(('192.168.0.200', 12345))
 	server_socket.listen(1)
 	print('Esperando conexión del cliente...')
 	client_socket, client_address = server_socket.accept()
 	print(f'Cliente conectado desde {client_address}')
 
-def dibujoCuadrado(tamano_casilla):
+def dibujoCuadrado(tamano_casilla, i):
 
 	skk.fillcolor(random.choice(list(colores.keys())))
 	skk.begin_fill()
@@ -89,7 +114,15 @@ def dibujoCuadrado(tamano_casilla):
 		skk.forward(tamano_casilla)
 		skk.right(90)
 	skk.end_fill()
+	skk.right(45)
+	skk.penup()
+	skk.fd(tamano_casilla/2)
+	skk.write(i)
+	skk.left(180)
+	skk.forward(tamano_casilla/2)
+	skk.right(135)
 	skk.forward(tamano_casilla)
+	skk.pendown()
 
 dibujarTablero()
 IniciarServidor()
