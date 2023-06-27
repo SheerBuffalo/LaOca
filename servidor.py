72302edd33f9c7bd2c95d5f06c005f7063bf632d
Perez Lopez Julio Armando <oiluj94@gmail.com>
2023-06-26 23:28:09 -0600

    waos

--- servidor.py
+++ servidor.py
@@ -2,12 +2,28 @@ import turtle #Inside_Out
 import random
 import socket
 
+boton_x =-10
+boton_y =-10
+botonLon =50  
+botonan =30
+
+player1_x=-200
+player1_y=-200
+
 wn = turtle.Screen()
 wn.bgcolor("light green")
 skk = turtle.Turtle()
 skk.penup()
 skk.goto(-200, -200)  # Posición inicial del tablero
 skk.pendown()
+skk.speed(40)
+jugador1 = turtle.Turtle()
+jugador1.color("red")
+jugador1.shape("triangle")
+jugador1.penup()
+jugador1.speed(0)
+jugador1.setposition(player1_x,player1_y)
+
 
 colores = {
     "red": "rojo",
@@ -93,6 +109,12 @@ def dibujarTablero():
 	skk.right(45)
 	skk.pendown()
 	skk.forward(100)
+	
+	draw_rect_button(skk)
+	wn.onclick(click)
+	turtle.done()
+	
+    
 
 def IniciarServidor():
 	tableroInicial=wn
@@ -124,11 +146,34 @@ def dibujoCuadrado(tamano_casilla, i):
 	skk.forward(tamano_casilla)
 	skk.pendown()
 
+def draw_rect_button(pen, message = 'dado'):
+    pen.penup()
+    pen.begin_fill()
+    pen.goto(boton_x, boton_y)
+    pen.goto(boton_x + botonLon, boton_y)
+    pen.goto(boton_x + botonLon, boton_y + botonan)
+    pen.goto(boton_x, boton_y + botonan)
+    pen.goto(boton_x, boton_y)
+    pen.end_fill()
+    pen.goto(boton_x + 15, boton_y + 15)
+    pen.write(message, font = ('Arial', 11, 'normal'))
+
+def click(x,y):
+	global mode
+	global player1_x
+	global player1_y
+	if boton_x<=x<=boton_x+botonLon:
+		if boton_y<=y<=boton_y+botonan:
+			player1_x=player1_x+5
+			jugador1.setposition(player1_x,player1_y)
+			print("alan weon")
+			
+
 dibujarTablero()
-IniciarServidor()
 
-
-
+speed=1
+while True:
+	skk.foward(speed)
 
 
 
